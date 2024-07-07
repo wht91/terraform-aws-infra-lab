92c399c9b823bb55e7201dfaee122c12fb168a7e
Wint Htet Thar <47767899+wht91@users.noreply.github.com>
2024-07-07 21:36:03 +0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  description = "Access key to AWS console"
+  default     = "xxxxx" # Add your access key
+}
+
+variable "secret_key" {
+  description = "Secret key to AWS console"
+  default     = "xxxxx" # Add your secret key
+}
