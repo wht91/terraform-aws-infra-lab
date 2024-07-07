5e3a614c8e1d97081c4d2aa25345d0a6d0021c5f
Wint Htet Thar <47767899+wht91@users.noreply.github.com>
2024-07-07 21:40:54 +0800

    Create database.tf

---
+++ database.tf
@@ -0,0 +1,29 @@
+# Create DB subnet group
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "db-subnet-group"
+  subnet_ids = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+
+  tags = {
+    Name = "DBSubnetGroup"
+  }
+}
+
+# Create RDS instance
+resource "aws_db_instance" "database" {
+  identifier                = "test-database" # Adjusted identifier to comply with naming rules
+  allocated_storage         = 20
+  storage_type              = "gp2"
+  engine                    = "mysql"
+  engine_version            = "8.0.35"
+  instance_class            = "db.t3.micro"
+  username                  = "xxxxx" Please add the database ussername
+  password                  = "xxxxx" # Please add the database password
+  db_subnet_group_name      = aws_db_subnet_group.db_subnet_group.name
+  vpc_security_group_ids    = [aws_security_group.rds_sg.id]
+  skip_final_snapshot       = true
+  final_snapshot_identifier = "final-test-database-snapshot" 
+
+  tags = {
+    Name = "TestDatabase"
+  }
+}
