36f6e1c78b5c21bd0a131d79ed850e3fbcadb2ad
Wint Htet Thar <47767899+wht91@users.noreply.github.com>
2024-07-08 19:51:49 +0800

    Update variables.tf for RDS password

--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
+# variables.tf
+
 variable "access_key" {
   description = "Access key to AWS console"
   default     = "xxxxx" # Add your access key
@@ -6,4 +8,14 @@ variable "access_key" {
 variable "secret_key" {
   description = "Secret key to AWS console"
   default     = "xxxxx" # Add your secret key
+}
+
+variable "rds_username" {
+  description = "RDS instance master username"
+  default     = "xxxxx" # Add your RDS username
+}
+
+variable "rds_password" {
+  description = "RDS instance master password"
+  default     = "xxxxx" # Add your RDS password
 }
