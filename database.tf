de85afc9de9872d2fd68d93ef94557637f4e128a
Wint Htet Thar <47767899+wht91@users.noreply.github.com>
2024-07-08 19:52:40 +0800

    Update database.tf

--- database.tf
+++ database.tf
@@ -16,8 +16,8 @@ resource "aws_db_instance" "database" {
   engine                    = "mysql"
   engine_version            = "8.0.35"
   instance_class            = "db.t3.micro"
-  username                  = "xxxxx" # Please add the database ussername
-  password                  = "xxxxx" # Please add the database password
+  username                  = var.rds_username
+  password                  = var.rds_password
   db_subnet_group_name      = aws_db_subnet_group.db_subnet_group.name
   vpc_security_group_ids    = [aws_security_group.rds_sg.id]
   skip_final_snapshot       = true
